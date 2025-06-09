0f1e0003a8c9c80c6c3d5d3c39a4171189c3b9f0
Killer66562 <jimmy688382@gmail.com>
2025-06-09 14:32:37 +0800

    Add person inner_code backup column

--- database/migrations/2025_06_07_064050_update_person_table.php
+++ database/migrations/2025_06_07_064050_update_person_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Person;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -12,9 +13,8 @@ return new class extends Migration
     public function up(): void
     {
         //
-        Schema::table('checklist', function (Blueprint $table) {
-            $table->dropColumn("inner_code");
-            $table->foreignId("person_id")->references("id")->on("person")->cascadeOnDelete()->cascadeOnUpdate();
+        Schema::table('person', function (Blueprint $table) {
+            $table->string("inner_code_backup");
         });
     }
 
@@ -24,9 +24,8 @@ return new class extends Migration
     public function down(): void
     {
         //
-        Schema::table('checklist', function (Blueprint $table) {
-            $table->dropConstrainedForeignId("person_id");
-            $table->string('inner_code', 20);
+        Schema::table('person', function (Blueprint $table) {
+            $table->dropColumn("inner_code_backup");
         });
     }
 };
